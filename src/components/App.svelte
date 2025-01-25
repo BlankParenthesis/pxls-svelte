6de69172cd5d64fd1ac5d7fd7b0b58c44f399ece
[  ] <blank.parenthesis@gmail.com>
2025-01-25 05:28:00 +0000

    Add url parameter to quick-load a specific board

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -7,12 +7,15 @@
 	import type { Reference } from "../lib/reference";
 	import Login from "./Login.svelte";
 	import Splash from "./layout/Splash.svelte";
-	import { persistentWritable } from "../lib/storage/persistent";
 	import { Settings } from "../lib/settings";
+	import { persistentWritable } from "../lib/storage/persistent";
+	import { urlWritable } from "../lib/storage/url";
 
 	const connecting = Site.connect(new URL(import.meta.env.VITE_TARGET_SITE));
 	let select: (board: Reference<BoardInfo>) => void;
 	const boardSelect = new Promise<Reference<BoardInfo>>(resolve => select = resolve);
+
+	let hotloadBoard = urlWritable("board", null, true);
 
 	let settings = persistentWritable(
 		"settings",
@@ -27,68 +30,80 @@
 		<progress max=1 value=0.1 />
 	</Splash>
 {:then site}
-	{#await site.defaultBoard()}
-		<Splash>
-			<p>Finding default board…</p>
-			<progress max=1 value=0.4 />
-		</Splash>
-	{:then defaultBoard}
-		{#await site.boards.get(defaultBoard.uri)}
+	{#if $hotloadBoard === null}
+		{#await site.defaultBoard()}
+			<Splash>
+				<p>Finding default board…</p>
+				<progress max=1 value=0.4 />
+			</Splash>
+		{:then defaultBoard}
+			{#await site.boards.get(defaultBoard.uri)}
+				<Splash>
+					<p>Loading board…</p>
+					<progress max=1 value=0.8 />
+				</Splash>
+			{:then board}
+				<Canvas {site} {board} bind:settings={$settings} />
+			{:catch e}
+				<Splash>
+					<h2>Loading error</h2>
+					<p>{e}</p>
+					<Login auth={site.auth}/>
+				</Splash>
+			{/await}
+		{:catch}
+			{#await collect(site.fetchBoards())}
+				<Splash>
+					<p>Loading board list…</p>
+					<progress max=1 value=0.6 />
+				</Splash>
+			{:then boards}
+				{#await boardSelect}
+					<div>
+						<ul>
+							{#each boards as board}
+								<li>
+									<BoardSelect info={board} on:select={b => select(b.detail)}/>
+								</li>
+							{/each}
+						</ul>
+						<hr />
+						<Login auth={site.auth}/>
+					</div>
+				{:then reference}
+					{#await site.boards.get(reference.uri)}
+						<Splash>
+							<p>Loading board…</p>
+							<progress max=1 value=0.8 />
+						</Splash>
+					{:then board}
+						<Canvas {site} {board} bind:settings={$settings} />
+					{:catch e}
+						<Splash>
+							<h2>Failed to load board</h2>
+							<p>{e}</p>
+						</Splash>
+					{/await}
+				{/await}
+			{:catch e}
+				<Splash>
+					<h2>Loading error</h2>
+					<p>{e}</p>
+					<Login auth={site.auth}/>
+				</Splash>
+			{/await}
+		{/await}
+	{:else}
+		<!-- hotload -->
+		{#await site.boards.get($hotloadBoard)}
 			<Splash>
 				<p>Loading board…</p>
 				<progress max=1 value=0.8 />
 			</Splash>
 		{:then board}
 			<Canvas {site} {board} bind:settings={$settings} />
-		{:catch e}
-			<Splash>
-				<h2>Loading error</h2>
-				<p>{e}</p>
-				<Login auth={site.auth}/>
-			</Splash>
 		{/await}
-	{:catch}
-		{#await collect(site.fetchBoards())}
-			<Splash>
-				<p>Loading board list…</p>
-				<progress max=1 value=0.6 />
-			</Splash>
-		{:then boards}
-			{#await boardSelect}
-				<div>
-					<ul>
-						{#each boards as board}
-							<li>
-								<BoardSelect info={board} on:select={b => select(b.detail)}/>
-							</li>
-						{/each}
-					</ul>
-					<hr />
-					<Login auth={site.auth}/>
-				</div>
-			{:then reference}
-				{#await site.boards.get(reference.uri)}
-					<Splash>
-						<p>Loading board…</p>
-						<progress max=1 value=0.8 />
-					</Splash>
-				{:then board}
-					<Canvas {site} {board} bind:settings={$settings} />
-				{:catch e}
-					<Splash>
-						<h2>Failed to load board</h2>
-						<p>{e}</p>
-					</Splash>
-				{/await}
-			{/await}
-		{:catch e}
-			<Splash>
-				<h2>Loading error</h2>
-				<p>{e}</p>
-				<Login auth={site.auth}/>
-			</Splash>
-		{/await}
-	{/await}
+	{/if}
 {:catch e}
 	<Splash>
 		<h2>Connection error</h2>
