8ac908da4c3b14f8bfb30b2860e411918ba7e198
[  ] <blank.parenthesis@gmail.com>
2025-01-30 12:12:52 +0000

    Use sourcemaps and multiple files in testing only

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [svelte(), viteSingleFile()],
-	build: { minify: true, sourcemap: true },
+export default defineConfig(({ command }) => {
+	const inProduction = command !== "serve";
+
+	const plugins = svelte() as Array<PluginOption>;
+
+	if (inProduction) {
+		plugins.push(viteSingleFile());
+	}
+
+	return {
+		plugins,
+		build: {
+			minify: true,
+			sourcemap: !inProduction,
+		},
+	};
 });
